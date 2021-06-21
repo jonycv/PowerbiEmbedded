67d7457e585f0d6d088eaec40aa02da0c6085ed7
Jonathan Cuéllar Viveros <jhonla@hotmail.com>
2021-06-21 18:07:31 +0200

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import * as pbi from 'powerbi-client';
+import { models } from 'powerbi-client';
+import { HttpClient } from '@angular/common/http';
+declare var powerbi: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'powerbiembed';
+ export class AppComponent implements OnInit  {
+
+  
+  constructor(private httpClient: HttpClient) { }
+
+  @ViewChild('embeddedReport')
+  embeddedReport!: ElementRef;
+  config: any;
+  screenHeight!: number;
+  screenWidth!: number;
+
+  ngOnInit() {
+    this.screenHeight = (window.screen.height);
+    this.screenWidth = (window.screen.width);    
+    this.httpClient.get<any>("https://localhost:44337/EmbedInfo")
+    .subscribe(config => {
+      this.config = config;
+      const model = pbi.models;
+      const embedConfig = {
+        type: 'report',
+        tokenType: model.TokenType.Embed,
+        accessToken: config.EmbedToken.Token,
+        embedUrl: config.EmbedReport[0].EmbedUrl,
+        permissions: model.Permissions.All,
+        settings: {
+          filterPaneEnabled: true,
+          navContentPaneEnabled: true
+        }
+      };
+      powerbi.embed(this.embeddedReport.nativeElement, embedConfig);
+    });
+  }
 }
